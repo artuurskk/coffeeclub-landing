5aacfd4d9237822153989ef79ee5e77db6f2f9ac
artuurskk <juan1337@inbox.lv>
2018-04-16 22:10:40 +0300

    v1

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link href='https://necolas.github.io/normalize.css/6.0.0/normalize.css' rel='stylesheet' type='text/css'>
+    <link href='css/style.css?ts=<?=time()?>' rel='stylesheet' type='text/css'>
+    <title>Coffee Club</title>
+</head>
+
+<body>
+    <div class = "container">
+      <header>
+        <div class = "logo"><img src="img/logo.png"></div>
+        <ul>
+          <li><a href="#">Sākums</a></li>
+          <li><a href="#">Par mums</a></li>
+          <li><a href="#">Cenas</a></li>
+          <li><a href="#">Kontakti</a></li>
+        </ul>
+      </header>
+    </div>
+      <div class= "welcome-panel">
+          <h1>Coffee Club - Labākā kafija Latvijā!</h1>
+          <h2>Mēs piedāvājam plašu kafijas veidu izvēli. Tas viss - par neticami zemām cenām!</h2>
+          <a href="#"><div class = "next-btn">Lasīt vairāk ▼</div></a>
+      </div>
+      <div class= "about-us">
+        <p>Mēs atrodamies pašā Rīgas centrā - <b>Gogoļa ielā 2.</b> <br>Pie mums var iegādāties ne tikai kafiju, bet arī dažādas uzkodas, smalkmaizītes, stipros dzērienus un daudz ko citu!<br>
+          Strādājam katru dienu - darba dienās no plkst 9:00 līdz 22:00, brīvdienās no plkst 10:00 līdz 21:00<br>
+          Laipni gaidīsim katru klientu!
+        </p>
+        <iframe width="600" height="450" frameborder="0"
+src="https://www.google.com/maps/embed/v1/place?q=place_id:EkFHb2dvxLxhIGllbGEgMiwgTGF0Z2FsZXMgcHJpZWvFoXBpbHPEk3RhLCBSxKtnYSwgTFYtMTA1MCwgTGF0dmlqYQ&key=AIzaSyBR76F5nes8ZHiQY9a1SZAZE77YUNqGQQI" allowfullscreen></iframe>
+      </div>
+      <div class= "coffee">
+        <h3>Mūsu klientu iecienītākās kafijas - TOP 5</h3>
+        <p>Visus piedāvājumus un cenas var apskatīt <b>šeit</b></p>
+        <ul>
+          <li><a href="#"><img src="img/espresso.png">Espresso</a></li>
+          <li><a href="#"><img src="img/latte.png">Caffe Latte</a></li>
+          <li><a href="#"><img src="img/cappuccino.png">Cappuccino</a></li>
+          <li><a href="#"><img src="img/auLait.png">Café au Lait</a></li>
+          <li><a href="#"><img src="img/muusu.png">Coffe Club Special</a></li>
+        </ul>
+      </div>
+      <footer>
+          Work in progress
+      </footer>
+</body>
+</html>
